61316ce0c9ff57741a7d0f9599a756f020ff9574
Valérian Fayt <valerian.fayt@epita.fr>
2020-05-28 01:21:14 +0200

    level: add attributes

--- src/level/level.hh
+++ src/level/level.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 #include "../mario/mario.hh"
 #include "../memory-band/memory-band.hh"
@@ -10,12 +11,17 @@ namespace interpretor
     class Level
     {
     public:
-        Level();
+        Level() = default;
 
         bool load(std::string path);
         bool play();
 
     private:
         Mario mario_;
+        MemoryBand memory_;
+        std::vector<std::string> board_;
+
+        bool set_start();
+        bool do_move(int x, int y);
     };
-}
+} // namespace interpretor

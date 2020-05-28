6da5b536eee5d98256e314d03a4911cef764df3d
Valérian Fayt <valerian.fayt@epita.fr>
2020-05-28 11:52:02 +0200

    level: add display

--- src/level/level.cc
+++ src/level/level.cc
@@ -1,4 +1,98 @@
 #include "level.hh"
 
 namespace interpretor
-{}
+{
+    bool Level::load(std::string path)
+    {
+        std::ifstream cf(path);
+
+        board_.resize(15);
+        size_t max_y = 0;
+        size_t max_x = 0;
+
+        for (max_y = 0; cf.good(); max_y++)
+        {
+            if (max_y + 1 > board_.size())
+                board_.resize(board_.size() * 2);
+
+            std::getline(cf, board_[max_y]);
+
+            if (board_[max_y].size() > max_x)
+                max_x = board_[max_y].size();
+        }
+
+        cf.close();
+
+        board_.resize(max_y - 1);
+
+        for (size_t i = 0; i < board_.size(); i++)
+            board_[i].resize(max_x, ' ');
+
+        return set_start();
+    }
+
+    bool Level::play()
+    {
+        return true;
+    }
+
+    bool Level::set_start()
+    {
+        return true;
+    }
+
+    bool Level::do_move(int x, int y)
+    {
+        return x != y;
+    }
+
+    void Level::display()
+    {
+        if (!board_.size())
+        {
+            std::cout << "# No map loaded #\n";
+            return;
+        }
+
+        for (size_t i = 0; i < board_[0].size() + 2; ++i)
+            std::cout << (i == board_[0].size() + 1 ? " \x1B[0m\n"
+                                                    : "\x1B[100m ");
+
+        for (size_t i = 0; i < board_.size(); ++i)
+        {
+            std::cout << "\x1B[100m \x1B[0m";
+
+            for (size_t j = 0; j < board_[i].size(); ++j)
+            {
+                if (mario_.pos_x_ == j && mario_.pos_y_ == i)
+                    std::cout << D_MARIO;
+
+                if (board_[i][j] == '(' || board_[i][j] == ')')
+                    std::cout << F_BLUE;
+                else if (board_[i][j] == '+')
+                    std::cout << F_GREEN;
+                else if (board_[i][j] == '-')
+                    std::cout << F_RED;
+                else if (board_[i][j] == '#' || board_[i][j] == '\"')
+                    std::cout << F_MAGENTA;
+                else if (board_[i][j] == '.' || board_[i][j] == ':'
+                         || board_[i][j] == ',' || board_[i][j] == ';')
+                    std::cout << F_CYAN;
+                else if (board_[i][j] == '^' || board_[i][j] == '!'
+                         || board_[i][j] == '[' || board_[i][j] == '@')
+                    std::cout << F_YELLOW;
+
+                std::cout << board_[i][j] << RST;
+            }
+
+            std::cout << "\x1B[100m \x1B[0m\n";
+        }
+
+        for (size_t i = 0; i < board_[0].size() + 2; ++i)
+            std::cout << (i == board_[0].size() + 1 ? " \x1B[0m\n"
+                                                    : "\x1B[100m ");
+
+        std::cout << std::endl;
+        memory_.display();
+    }
+} // namespace interpretor
--- src/level/level.hh
+++ src/level/level.hh
@@ -1,10 +1,24 @@
 #pragma once
 
+#include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
 
 #include "../mario/mario.hh"
 #include "../memory-band/memory-band.hh"
+
+/* COLORS */
+#define RST "\x1B[0m"
+#define F_RED "\x1B[31m"
+#define F_GREEN "\x1B[32m"
+#define F_YELLOW "\x1B[33m"
+#define F_BLUE "\x1B[34m"
+#define F_MAGENTA "\x1B[35m"
+#define F_CYAN "\x1B[36m"
+#define F_WHITE "\x1B[37m"
+#define F_BLACK "\x1B[90m"
+#define D_MARIO "\033[7m"
 
 namespace interpretor
 {
@@ -15,6 +29,7 @@ namespace interpretor
 
         bool load(std::string path);
         bool play();
+        void display();
 
     private:
         Mario mario_;

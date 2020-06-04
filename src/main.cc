f46b22059cbc03d408476a9d008cf105ba5333f1
Valérian Fayt <valerian.fayt@epita.fr>
2020-06-04 20:36:40 +0200

    option: parse option

--- src/main.cc
+++ src/main.cc
@@ -1,13 +1,22 @@
 #include <iostream>
+#include <optional>
 
 #include "level/level.hh"
+#include "option/option.hh"
 
 int main(int argc, char* argv[])
 {
+    auto options = option_parser::parse_options(argc, argv);
+
+    if (!options.has_value())
+        return 1;
+
     interpretor::Level level = interpretor::Level();
 
-    level.load("file.mlg");
-    level.play();
+    if (level.load(options.value().path_))
+        level.play();
+    else
+        std::cerr << "Please select a valid file.";
 
     return 0;
 }
